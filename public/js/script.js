d47e64f33ae753185a0483f9d4e4eff2b57375be
simaonogueira101 <simao.nogueira66@gmail.com>
2018-02-21 16:33:51 +0000

    Styles animations.

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,31 @@
+var ind = 1;
+
 document.querySelector('#hello').addEventListener('click', function(){
+  var heroHeight = document.querySelector('section.hero .hero-body').offsetHeight;
+  var heroContainerMarginLeft = document.querySelector('section.hero .hero-body .container').offsetLeft;
+  console.log(heroContainerMarginLeft);
+
+  document.querySelector('.section-about').style.marginLeft = heroContainerMarginLeft - 24 + "px";
+
+  var aboutHeight = document.querySelector('.section-about').offsetHeight;
+
+  var contentHeight = heroHeight + aboutHeight;
+
   document.querySelector('section.hero .hero-body').classList.toggle("active");
+
+  if(ind === 1) {
+    document.querySelector('.section-about').style.top = heroHeight - 48 + "px";
+    if(window.innerHeight > contentHeight) {
+      document.querySelector('section.hero .hero-body').style.minHeight = "100vh";
+    } else {
+      document.querySelector('section.hero .hero-body').style.minHeight = contentHeight + "px";
+    }
+    ind = 0;
+  } else {
+    document.querySelector('.section-about').style.top = "-200vh";
+    document.querySelector('section.hero .hero-body').style.minHeight = "0";
+    ind = 1;
+  }
+
   document.querySelector('.section-about').classList.toggle("active");
 });
